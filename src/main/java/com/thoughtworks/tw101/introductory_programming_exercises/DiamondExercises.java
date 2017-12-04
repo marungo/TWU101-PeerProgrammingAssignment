ed6de3a8ba475adbc94d65a9300e2215944cae2e
Shlok Amin <shlok.amin1994@gmail.com>
2017-12-03 22:35:34 -0800

    finished drawAnIsoscelesTriangle function

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.tw101.introductory_programming_exercises;
 
+import com.intellij.jarRepository.services.artifactory.Endpoint;
+
 public class DiamondExercises {
     public static void main(String[] args) {
         drawAnIsoscelesTriangle(3);
@@ -12,8 +14,41 @@ public class DiamondExercises {
 //              *
 //             ***
 //            *****
-    private static void drawAnIsoscelesTriangle(int n) {
+//
+//              *
+//             ***
+//            *****
+//           ******* n = 6
+//          *********
+//         ***********
 
+    private static void drawAnIsoscelesTriangle(int n) {
+        StringBuffer[] triangle = new StringBuffer[n];
+        for (int i = 0; i<n ; i++) {
+            int numSpaces = n-i-1;  //empty spaces increase by one starting from bottom row to top
+            int numStars = (2*i)+1; //always the odd numbers
+
+            StringBuffer currRow = new StringBuffer();
+            addEmptySpaces(numSpaces,currRow);
+            addStars(numStars,currRow);
+            triangle[i] = currRow;
+        }
+
+        for (int i = 0; i <triangle.length ; i++) {
+            System.out.println(triangle[i]);
+        }
+    }
+
+    private static void addEmptySpaces(int spaces, StringBuffer currRow){
+        for (int j = 0; j < spaces ; j++) {
+            currRow.append(" "); //  add the blank space
+        }
+    }
+
+    private static void addStars(int numStars, StringBuffer currRow) {
+        for (int i = 0; i < numStars ; i++) {
+            currRow.append("*");
+        }
     }
 
 //    Diamond
@@ -24,8 +59,20 @@ public class DiamondExercises {
 //             ***
 //              *
     private static void drawADiamond(int n) {
+        StringBuffer topOfDiamond = new StringBuffer();
+        for (int i = 0; i <n ; i++) {
+            StringBuffer currRowOfStars = new StringBuffer();
 
-    }
+
+
+
+
+            }
+
+
+        }
+
+
 
 //    Diamond with Name
 //    Given a number n, print a centered diamond with your name in place of the middle line. Example for n=3:
