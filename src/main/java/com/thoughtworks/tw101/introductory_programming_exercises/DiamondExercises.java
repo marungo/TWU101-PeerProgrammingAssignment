0c67bf0fbcfce0ccfa074afc1e1a00b1b2177012
Shlok Amin <shlok.amin1994@gmail.com>
2017-12-04 13:48:31 -0800

    completed diamond exercises

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -15,39 +15,11 @@ public class DiamondExercises {
 //             ***
 //            *****
 //
-//              *
-//             ***
-//            *****
-//           ******* n = 6
-//          *********
-//         ***********
 
     private static void drawAnIsoscelesTriangle(int n) {
-        StringBuffer[] triangle = new StringBuffer[n];
-        for (int i = 0; i<n ; i++) {
-            int numSpaces = n-i-1;  //empty spaces increase by one starting from bottom row to top
-            int numStars = (2*i)+1; //always the odd numbers
-
-            StringBuffer currRow = new StringBuffer();
-            addEmptySpaces(numSpaces,currRow);
-            addStars(numStars,currRow);
-            triangle[i] = currRow;
-        }
-
+        StringBuffer[] triangle = getIsoscelesTriangle(n);
         for (int i = 0; i <triangle.length ; i++) {
             System.out.println(triangle[i]);
-        }
-    }
-
-    private static void addEmptySpaces(int spaces, StringBuffer currRow){
-        for (int j = 0; j < spaces ; j++) {
-            currRow.append(" "); //  add the blank space
-        }
-    }
-
-    private static void addStars(int numStars, StringBuffer currRow) {
-        for (int i = 0; i < numStars ; i++) {
-            currRow.append("*");
         }
     }
 
@@ -58,20 +30,17 @@ public class DiamondExercises {
 //            *****
 //             ***
 //              *
+
     private static void drawADiamond(int n) {
-        StringBuffer topOfDiamond = new StringBuffer();
-        for (int i = 0; i <n ; i++) {
-            StringBuffer currRowOfStars = new StringBuffer();
+        StringBuffer[] triangle = getIsoscelesTriangle(n);
 
-
-
-
-
-            }
-
-
+        for (int i = 0; i <triangle.length ; i++) {
+            System.out.println(triangle[i]);
         }
-
+        for (int i = (triangle.length) - 2; i>=0; i--) {
+            System.out.println(triangle[i]);
+        }
+    }
 
 
 //    Diamond with Name
@@ -84,5 +53,43 @@ public class DiamondExercises {
 //             *
     private static void drawADiamondWithYourName(int n) {
 
+        StringBuffer[] triangle = getIsoscelesTriangle(n-1);
+        for (int i = 0; i <triangle.length ; i++) {
+            System.out.println(triangle[i]);
+        }
+
+        System.out.println("shlok");
+
+        for (int i = (triangle.length) - 1; i>=0; i--) {
+            System.out.println(triangle[i]);
+        }
+
+    }
+
+    private static StringBuffer[] getIsoscelesTriangle(int n) {
+        StringBuffer[] triangle = new StringBuffer[n];
+        for (int i = 0; i<n ; i++) {
+            int numSpaces = n-i-1;  //empty spaces increase by one starting from bottom row to top
+            int numStars = (2*i)+1; //always the odd numbers
+
+            StringBuffer currRow = new StringBuffer();
+            addEmptySpaces(numSpaces,currRow);
+            addStars(numStars,currRow);
+            triangle[i] = currRow;
+        }
+        return triangle;
+    }
+
+
+    private static void addEmptySpaces(int spaces, StringBuffer currRow){
+        for (int j = 0; j < spaces ; j++) {
+            currRow.append(" "); //  add the blank space
+        }
+    }
+
+    private static void addStars(int numStars, StringBuffer currRow) {
+        for (int i = 0; i < numStars ; i++) {
+            currRow.append("*");
+        }
     }
 }
