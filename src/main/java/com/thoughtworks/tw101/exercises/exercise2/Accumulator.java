c5b144f37c6bcc9d4cc2db83b184a1e6667de574
Shlok Amin <shlok.amin1994@gmail.com>
2017-12-07 11:55:27 -0800

    Finished exercise 2

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,12 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+    int incrementCount = 0;
+    void increment(){
+        incrementCount++;
+    }
+
+    void total() {
+        System.out.println("increment() called " + incrementCount + " times.");
+    }
 }
