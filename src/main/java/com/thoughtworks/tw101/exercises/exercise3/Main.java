f30e7e13873dd642256a289654652e8e61860aa8
Shlok Amin <shlok.amin1994@gmail.com>
2017-12-11 11:36:38 -0800

    finished exercise3. Assuming 'between' is exclusive

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/Main.java
@@ -5,11 +5,12 @@ public class Main {
 //    Exercise #3: Create a function 'of' in the class OddSum that returns the sum of all odd integers between a starting
 //    and ending integer value.
 
+// Note: assuming "between" is exlusive - Shlok Amin
+
+
     public static void main(String[] args) {
         OddSum oddSum = new OddSum();
-
         Integer sum = oddSum.of(1, 100);
-
         System.out.println(sum);
     }
 }
