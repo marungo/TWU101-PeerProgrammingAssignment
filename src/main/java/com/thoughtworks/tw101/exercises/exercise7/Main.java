575c86f7fb280737d1a6e7b8611e096c136bbec5
Shlok Amin <shlok.amin1994@gmail.com>
2017-12-18 14:32:44 -0800

    finished exercise 7. Note: only one class used

--- src/main/java/com/thoughtworks/tw101/exercises/exercise7/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise7/Main.java
@@ -4,9 +4,18 @@ package com.thoughtworks.tw101.exercises.exercise7;
 // right then tell them they win, otherwise tell them if they guessed too high or low. Keep asking the user to guess
 // until they get the right answer. Use classes to separate the different concerns of this program.
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
-
     public static void main(String[] args) {
-
+        guessingGame game = new guessingGame();
+        while (!game.gameOver){
+            game.promptUserForGuess();
+            game.compare();
+        }
     }
 }
+
+
+
---
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise7/guessingGame.java
@@ -0,0 +1,85 @@
+package com.thoughtworks.tw101.exercises.exercise7;
+
+import java.util.Random;
+import java.util.Scanner;
+
+//on fail make sure to close scanner
+
+public class guessingGame {
+    private int randomNumber;
+    private Scanner scanner;
+    private int guess;
+    public boolean gameOver = false;
+
+    guessingGame() {
+        randomNumber = getRandomNumber();
+        scanner = new Scanner(System.in);
+    }
+
+
+    public int getRandomNumber() {
+        Random r = new Random();
+        int random = r.nextInt(99);
+        random += 1;
+        return random;
+    }
+
+    public void promptUserForGuess() {
+        askForGuess();
+        recieveGuess();
+    }
+    void recieveGuess() {
+        boolean validInput = false;
+        int potentialGuess;
+        do {
+            if (scanner.hasNextInt()) {
+                potentialGuess = scanner.nextInt();
+                if (isWithinValidRange(potentialGuess)) {
+                    guess = potentialGuess;
+                    validInput = true;
+                }
+                else {
+                    invalidPosInt();
+                }
+            }
+            else {
+                invalidPosInt();
+            }
+        } while (!validInput);
+        System.out.println(guess);
+
+    }
+
+    void compare(){
+        if (guess == randomNumber) {
+            System.out.println("you guessed right! you win!");
+            gameOver = true;
+            return;
+        }
+        if (guess>randomNumber){
+            System.out.println("you guessed too high!");
+        }
+        else{
+            System.out.println("you guessed too low!");
+        }
+    }
+
+    boolean isWithinValidRange(int potentialGuess) {
+        if (potentialGuess > 0 && potentialGuess < 100) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    void askForGuess() {
+        System.out.println("Please enter a valid positive integer");
+    }
+
+    void invalidPosInt() {
+        askForGuess();
+        scanner.next();
+    }
+}
+
